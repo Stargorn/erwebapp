de14f582df983799e3db42faa359d5d38397f51c
daniel <daniel_da_paniel@hotmail.com>
2018-10-14 03:32:12 +0000

    Set up models for projects, attached to admin, created ourwork.html but the view is not returning an HttpResponse object.

--- erwebapp/urls.py
+++ erwebapp/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('basesite.urls')),
+    path('', include('projects.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    thumb_image = models.ImageField(upload_to='img', null = True)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='img', null = True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    
+class TimeLine(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+class Note(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    date = models.DateTimeField('date written')
+    note = models.CharField(max_length=2000)
